1e6538f21e71f114fbc60eafffeed620e720b430
Laskaridi <anarizhko@gmail.com>
2024-09-13 09:05:09 +0300

    Lesson №1

---
+++ 01/main.js
@@ -0,0 +1,25 @@
+// alert("Hello world");
+
+let userNickName = "Student";
+let userScore = 125;
+let age = 35;
+
+console.log(age);
+
+resultAge = age + 5;
+console.log(resultAge);
+age = 40;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+isStudent = false;
+console.log(isStudent);
+
+let item = 10;
+console.log(item);
+item = "ten";
+console.log(item);
+item = true;
+console.log(item);
