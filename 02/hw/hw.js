ead3eb04953c2dbe07508c0fb51b3e2f8c746e00
Laskaridi <anarizhko@gmail.com>
2024-09-18 08:17:51 +0300

    Lesson №2

---
+++ 02/hw/hw.js
@@ -0,0 +1,17 @@
+let number1 = 23, number2 = 41;
+console.log(`Compare number1 and number2: ${number1==number2}`);
+console.log(`Compare number1 and number2: ${number1!=number2}`);
+alert(`Number1 and number2: ${number1==number2}`);
+
+
+let widthRest = 47, hightRest = 24;
+console.log(`Area of a restangle: ${widthRest*hightRest}`);
+console.log(`Perimeter of a ristangle: ${(widthRest+hightRest)*2}`);
+
+
+let tempC = 25;
+console.log(`TemperatureF: ${tempC*(9/5)+32}`);
+
+
+let radiusCyl = 14.3, hightCyl = 23.6;
+console.log(`Volume of a cylinder: ${3.14*(radiusCyl**2)*hightCyl}`)
