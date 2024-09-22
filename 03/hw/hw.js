6a5531998af56f1bfca97a46fa930a83c1a04608
Laskaridi <anarizhko@gmail.com>
2024-09-22 12:56:04 +0300

    Lesson №3

---
+++ 03/hw/hw.js
@@ -0,0 +1,166 @@
+// №1
+let index1 = Number(prompt('Введіть число'));
+if(index1%2 === 0){
+    console.log(`Число парне`);
+}
+else{
+    console.log(`Число непарне`);
+}
+
+
+//№2
+let userAge = Number(prompt('Введіть ваш вік'));
+if(userAge >=18){
+    alert('Ви можете використовувати цей ресурс');
+}
+else{
+    alert('Ви не можете використовувати цей ресурс');
+}
+
+
+
+//№3
+let priceProduct = Number(prompt('Введіть ціну товару'));
+let quantityProduct = Number(prompt('Введіть кількість товару'));
+console.log(priceProduct*quantityProduct);
+
+
+
+//№4
+let price2Product = Number(prompt('Введіть ціну товару'));
+alert((price2Product/100)*50);
+
+
+//№5
+let number1 = Number(prompt('Введіть перше число'));
+let number2 = Number(prompt('Введіть друге число'));
+let number3 = Number(prompt('Введіть третє число'));
+
+if(number1>number2 && number1>number3){
+    let maxValue;
+    console.log(maxValue = number1);
+}
+ else if(number2>number1 && number2>number3){ 
+        console.log(maxValue = number2);
+    }
+  else if(number3>number1 && number2>number2){
+        console.log(maxValue = number3);
+}
+else{
+    console.log('Усі числа рівні');
+}
+
+
+//№6
+let month = Number(prompt('Введіть номер місяця'));
+switch(month){
+    case 1:
+    case 2:
+    case 12:
+        console.log('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Літо');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осінь');
+        break;
+}
+
+
+
+//№7
+let index2 = Number(prompt('Введіть число'));
+if(index2>0){
+    console.log('Число додатнє');
+}
+else if(index2<0){
+    console.log("Число від'ємне");
+}
+else{
+    console.log("Число нуль");
+}
+
+
+
+//№8
+let figure = prompt('Введіть назву геометрічної фігури');
+switch(figure){
+    case "коло":
+        console.log('Ця фігура має 0 кутів');
+        break;
+    case "квадрат":
+        console.log('Ця фігура має 4 кути');
+        break;
+    case "трикутник":
+        console.log('Ця фігура має 3 кути');
+        break;
+}
+
+
+
+//№9
+let counter = 1;
+let sum = 0;
+while(counter<20){
+    if(counter%2 === 0){
+        sum += counter;
+        counter++;
+    }
+    else{
+        counter++;
+    }
+}
+console.log(`Сума парних чисел: ${sum}`);
+
+
+//№10
+for(let index3 = 10; index3 >= 1; index3--){
+    console.log(`Зворотній лічильник: ${index3}`);
+}
+
+
+
+//№11
+let sizeAngle = Number(prompt('Введіть величину кута в градусах'));
+let size2 = '';
+size2 =  sizeAngle > 90 ? 'Цей кут тупий' : sizeAngle < 90 ? 'Цей кут гострий' : 'Цей кут прямий';
+console.log(size2);
+
+
+
+//№12
+for(let counter2 = 1; counter2 <=10; counter2++){
+   if(counter2%2 === 0){
+    console.log(`Перше парне число: ${counter2}`);
+   break;
+   }
+}
+
+
+
+//№13
+let planet = prompt('Введіть назву планети');
+switch(planet){
+    case "меркурій":
+    case "венера":
+    case "земля":
+    case "марс":
+    case "юпітер":
+    case "сатурн":
+    case "уран":
+    case "нептун":
+        console.log("Це планета сонячної системи");
+        break;
+        default:
+            console.log("Такої планети немає в сонячній системі");
+}
---
+++ 03/hw2.js
@@ -0,0 +1,39 @@
+let weight1 = 72, weight2 = 82, weight3 = 50, weight4 = 62;
+let height1 = 1.88, height2 = 1.73, height3 = 1.58, height4 = 1.70;
+let sum1 = weight1/height1**2;
+let sum2 = weight2/height2**2;
+let sum3 = weight3/height3**2;
+let sum4 = weight4/height4**2;
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+console.log(sum1==sum2);
+console.log(sum3==sum4);
+
+let maxValue
+if(sum1>sum2, sum1>sum3, sum1>sum4){
+    console.log(maxValue = sum1);
+}
+else if(sum2>sum1, sum2>sum3, sum2>sum4){
+    console.log(maxValue = sum2);
+}
+else if(sum3>sum1, sum2>sum2, sum2>sum4){
+    console.log(maxValue = sum3);
+}
+else{
+    console.log(maxValue = sum4);
+}
+
+let crazyKats1 = 44, crazyKats2 = 23, crazyKats3 = 71;
+let funnyDucks1 = 65, funnyDucks2 = 54, funnyDucks3 = 49;
+let crazyKats, funnyDucks;
+console.log(crazyKats = (crazyKats1 + crazyKats2 + crazyKats3)/3);
+console.log(funnyDucks = (funnyDucks1 + funnyDucks2 + funnyDucks3)/3);
+let sum5;
+if(crazyKats/funnyDucks){
+    console.log(sum5 = crazyKats);
+}
+else {
+    console.log(sum5 = funnyDucks);
+}
