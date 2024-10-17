06439384c99679f1bc7c407c387b9a22855c38e9
Laskaridi <anarizhko@gmail.com>
2024-10-17 09:47:56 +0300

    Delete files

--- 09/hw/script/page3.js
+++ 09/hw/script/page3.js
@@ -27,19 +27,21 @@ let blockEl = document.createElement('section');
 bodyContainer.appendChild(blockEl);
 blockEl.setAttribute('style', 'display:flex;flex-wrap:wrap');
 
+
+function getRandomColor(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
 for(let i = 0; i < 50; i++){
 let divEl = document.createElement('div');
 divEl.setAttribute('style', 'border-radius:50%;width:50px;height:50px;background-color:green');
 divEl.setAttribute('class', 'circle-element');
+divEl.style.backgroundColor = getRandomColor();
 blockEl.appendChild(divEl);
 }
 
 
-let bgColor = () => {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    if (randomColor.length<6) randomColor = randomColor.padStart(6,"0");
-    divEl.style.backgroundColor = "#" + randomColor;
-    output.innerHTML = "#" + randomColor;
-  }
-   
-  bgColor();
